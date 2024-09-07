f2e3d09ca1b143b88c53fd2a63250050cad8d954
Jaideep Bose <jaideepbose51@gmail.com>
2024-09-07 14:48:17 +0530

    frontend

--- server/app.js
+++ server/app.js
@@ -15,6 +15,14 @@ dbconnect();
 app.use(express.json());
 app.use(morgan());
 
+import cors from 'cors';
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+
 app.use("/user", userRouter);
 app.use("/chat",chatRouter);
 
@@ -23,3 +31,4 @@ app.listen(port,(err)=>{
     if (err) throw err;
     console.log(`Server is running on port ${port}`);
 })
+
